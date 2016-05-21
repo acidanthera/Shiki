4bbc8bd89e2a92c4909ca5ca0a8c453a99d86039
vit9696 <vit9696@users.noreply.github.com>
2016-05-21 09:37:35 +0400

    Add FAQ

--- BoardHash/BoardHash.c
+++ BoardHash/BoardHash.c
@@ -4,16 +4,16 @@
 
 void calcHash(const char * board) {
 	CC_SHA1_CTX ctx;
-    size_t len = strlen(board) + 1;
-    CC_SHA1_Init(&ctx);
-    CC_SHA1_Update(&ctx, board, len);
+	size_t len = strlen(board) + 1;
+	CC_SHA1_Init(&ctx);
+	CC_SHA1_Update(&ctx, board, len);
 	unsigned char md[CC_SHA1_DIGEST_LENGTH];
-    CC_SHA1_Final(md, &ctx);
-    printf("board-id hash: ");
-    for (size_t i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
-            printf("%02x", md[i]);
-    }
-    printf(" = %zu\n", len);
+	CC_SHA1_Final(md, &ctx);
+	printf("board-id hash: ");
+	for (size_t i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
+			printf("%02x", md[i]);
+	}
+	printf(" = %zu\n", len);
 }
 
 int main(int argc, char *argv[]) {
