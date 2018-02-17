6cc06fc96df7173985af6a1241c8afaed5584bc8
vit9696 <vit9696@users.noreply.github.com>
2018-02-17 17:54:29 +0300

    Rework renderer compatibility patch to support and Ivy and Sandy (first attempt)

--- Shiki/kern_start.cpp
+++ Shiki/kern_start.cpp
@@ -2,188 +2,170 @@
 //  kern_start.cpp
 //  Shiki
 //
-//  Copyright © 2016-2017 vit9696. All rights reserved.
+//  Copyright © 2016-2018 vit9696. All rights reserved.
 //
 
 #include <Library/LegacyIOService.h>
 #include <Headers/plugin_start.hpp>
 #include <Headers/kern_api.hpp>
+#include <Headers/kern_cpu.hpp>
 #include <Headers/kern_file.hpp>
 #include <Headers/kern_iokit.hpp>
 #include <IOKit/IODeviceTreeSupport.h>
 
 #include "kern_resources.hpp"
 
+// Aside generic DRM unlock patches, which are always on, Shiki also provides a set of patches
+// to workaround various issues with hardware video acceleration support.
+// These are set as a shikigva boot-arg bitmask.
+// For example, to enable ForceOnlineRenderer, ExecutableWhitelist, and ReplaceBoardID
+// you sum 1 + 8 + 32 = 41 -> and pass shikigva=41.
 enum ShikiGVAPatches {
-	ForceOnlineRenderer = 1,
-	AllowNonBGRA = 2,
-	ForceCompatibleRenderer = 4,
-	VDAExecutableWhitelist = 8,
+	// Remove forceOfflineRenderer from AppleGVA Info.plist.
+	// This is required to allow hardware acceleration on several mac models with discrete GPUs
+	// when only IGPU is available.
+	// See /System/Library/PrivateFrameworks/AppleGVA.framework/Resources/Info.plist for mor details.
+	ForceOnlineRenderer        = 1,
+	// Remve hweBGRA from AppleGVA Info.plist.
+	// hweBGRA is not supported by NVIDIA GPUs, so the patch is sometimes required when using NVIDIA
+	// in a mac model meant to be used with AMD or Intel.
+	// See /System/Library/PrivateFrameworks/AppleGVA.framework/Resources/Info.plist for mor details.
+	AllowNonBGRA               = 2,
+	// Some GPU+CPU combinations are not meant to provide hardware acceleration and need to be patched.
+	// Fix hardware acceleration with NVIDIA+SKL, NVIDIA+KBL, AMD+IVB, NVIDIA+SNB.
+	ForceCompatibleRenderer    = 4,
+	// Unlike 10.12 and earlier, on 10.13 AppleGVA patches do not apply to all processes, and each
+	// process needs to be patched explicitly. This bit ensures that processes present in WHITELIST
+	// section of Patches.plist will definitely get the fixes even on 10.13.
+	// On 10.13 this must be used if any of the following bits are used:
+	// - ForceOnlineRenderer
+	// - AllowNonBGRA
+	// - ForceCompatibleRenderer
+	// - ReplaceBoardID
+	AddExecutableWhitelist     = 8,
+	// Disable hardware-accelerated FairPlay support in iTunes to fix crashes in 10.13.
+	// While this breaks streaming, it is currently the only way to workaround iTunes crashes in 10.13
+	// when one has IGPU installed.
+	// This is the only bit that is enabled automatically on 10.13 if shikigva is *NOT* passed via boot-args.
 	DisableHardwareKeyExchange = 16,
-	ReplaceBoardID = 32
+	// Replace board-id used by AppleGVA by a different board-id.
+	// Sometimes it is feasible to use different GPU acceleration settings from the main mac model.
+	// By default Mac-27ADBB7B4CEE8E61 (iMac14,2) will be used, but you can override this via shiki-id boot-arg.
+	// See /System/Library/PrivateFrameworks/AppleGVA.framework/Resources/Info.plist for mor details.
+	ReplaceBoardID             = 32,
+	// Attempt to support fps.1_0 (FairPlay 1.0) in Safari.
+	// This should technically fix some very old streaming services in Safari, which rely on FairPlay DRM
+	// similar to the one found in iTunes. Newer streaming services require FairPlay 2.0, which is hardware-only,
+	// so nothing could be done about them.
+	// Another way to enable this is to pass -shikifps boot argument.
+	UnlockFP10Streaming        = 64
 };
-
-// 32 bytes should be reasonable for a safe comparison
-static constexpr size_t NVPatchSize {32};
-static uint8_t nvPatchFind[NVPatchSize] {};
-static uint8_t nvPatchReplace[NVPatchSize] {};
-static UserPatcher::BinaryModPatch *nvPatch {nullptr};
 
 static bool autodetectIGPU {false};
 static char customBoardID[64] {};
 
 static void disableSection(uint32_t section) {
 	for (size_t i = 0; i < ADDPR(procInfoSize); i++) {
-		if (ADDPR(procInfo)[i].section == section) {
+		if (ADDPR(procInfo)[i].section == section)
 			ADDPR(procInfo)[i].section = SectionUnused;
-		}
 	}
 	
 	for (size_t i = 0; i < ADDPR(binaryModSize); i++) {
 		auto patches = ADDPR(binaryMod)[i].patches;
 		for (size_t j = 0; j < ADDPR(binaryMod)[i].count; j++) {
-			if (patches[j].section == section) {
+			if (patches[j].section == section)
 				patches[j].section = SectionUnused;
-			}
 		}
 	}
 }
 
-static uint32_t getModernFrameId() {
-	uint32_t platform = 0;
-	const char *tree[] {"AppleACPIPCI", "IGPU"};
-	auto sect = WIOKit::findEntryByPrefix("/AppleACPIPlatformExpert", "PCI", gIOServicePlane);
-	for (size_t i = 0; sect && i < arrsize(tree); i++) {
-		sect = WIOKit::findEntryByPrefix(sect, tree[i], gIOServicePlane);
-		if (sect && i+1 == arrsize(tree)) {
-			if (WIOKit::getOSDataValue(sect, "AAPL,ig-platform-id", platform)) {
-				DBGLOG("audio", "found IGPU with ig-platform-id %08x", platform);
-				return platform;
-			} else {
-				SYSLOG("audio", "found IGPU with missing ig-platform-id, assuming old");
+static bool shikiSetCompatibleRendererPatch() {
+	// Support 10.10 and higher for now.
+	if (getKernelVersion() < KernelVersion::Yosemite)
+		return false;
+
+	// Let's look the patch up first.
+	UserPatcher::BinaryModPatch *compPatch {nullptr};
+	for (size_t i = 0; i < ADDPR(binaryModSize); i++) {
+		auto patches = ADDPR(binaryMod)[i].patches;
+		for (size_t j = 0; j < ADDPR(binaryMod)[i].count; j++) {
+			if (patches[j].section == SectionCOMPATRENDERER) {
+				compPatch = &patches[j];
+				DBGLOG("shiki", "found compat renderer patch at %lu:%lu with size %lu", i, j, compPatch->size);
+				break;
 			}
 		}
 	}
 
-	DBGLOG("audio", "failed to find IGPU ig-platform-id");
-	return platform;
+	if (!compPatch)
+		return false;
+
+	// I will be frank, the register could change here. But for a good reason it did not for some time.
+	// This patch is much simpler than what we had before, so let's stick to it for the time being.
+
+	// lea eax, [rdx-1080000h]
+	static uint8_t yosemitePatchFind[] {0x8D, 0x82, 0x00, 0x00, 0xF8, 0xFE};
+	static uint8_t yosemitePatchReplace[] {0x8D, 0x82, 0x00, 0x00, 0xF8, 0xFE};
+	static constexpr size_t YosemitePatchOff = 2;
+
+	// lea r10d, [r9-1080000h]
+	static uint8_t sierraPatchFind[] {0x45, 0x8D, 0x91, 0x00, 0x00, 0xF8, 0xFE};
+	static uint8_t sierraPatchReplace[] {0x45, 0x8D, 0x91, 0x00, 0x00, 0xF8, 0xFE};
+	static constexpr size_t SierraPatchOff = 3;
+
+	if (getKernelVersion() >= KernelVersion::Sierra) {
+		compPatch->find = sierraPatchFind;
+		compPatch->find = sierraPatchReplace;
+		compPatch->size = sizeof(sierraPatchFind);
+	} else {
+		compPatch->find = yosemitePatchFind;
+		compPatch->find = yosemitePatchReplace;
+		compPatch->size = sizeof(yosemitePatchFind);
+	}
+
+	// Here are all the currently valid IOVARendererID values:
+	// 0x1080000 — Sandy Bridge
+	// 0x1080001 — Sandy Bridge (not used by the drivers)
+	// 0x1080002 — Ivy Bridge
+	// 0x1080004 — Haswell
+	// 0x1080008 — Broadwell
+	// 0x1080010 — Skylake
+	// 0x1080020 — Kaby Lake
+	// 0x1020000 — AMD prior to 5xxx (for example, Radeon HD 2600)
+	// 0x1020002 — AMD 5xxx and newer
+	// 0x1040002 — NVIDIA VP2
+	// 0x1040004 — NVIDIA VP3
+	// 0x1040008 — NVIDIA VP4 and newer
+	// More details are outlined in https://www.applelife.ru/posts/716793.
+
+	// This patch makes AppleGVA believe that we use Haswell, which is not restricted to any modern GPU
+	auto generation = CPUInfo::getGeneration();
+	if (generation == CPUInfo::CpuGeneration::SandyBridge) {
+		*reinterpret_cast<int32_t *>(&yosemitePatchReplace[YosemitePatchOff]) += (0x1080000 - 0x1080004);
+		*reinterpret_cast<int32_t *>(&sierraPatchReplace[SierraPatchOff])     += (0x1080000 - 0x1080004);
+		return true;
+	} else if (generation == CPUInfo::CpuGeneration::IvyBridge) {
+		*reinterpret_cast<int32_t *>(&yosemitePatchReplace[YosemitePatchOff]) += (0x1080002 - 0x1080004);
+		*reinterpret_cast<int32_t *>(&sierraPatchReplace[SierraPatchOff])     += (0x1080002 - 0x1080004);
+		return true;
+	} else if (generation == CPUInfo::CpuGeneration::Skylake) {
+		*reinterpret_cast<int32_t *>(&yosemitePatchReplace[YosemitePatchOff]) += (0x1080010 - 0x1080004);
+		*reinterpret_cast<int32_t *>(&sierraPatchReplace[SierraPatchOff])     += (0x1080010 - 0x1080004);
+		return true;
+	} else if (generation == CPUInfo::CpuGeneration::KabyLake) {
+		*reinterpret_cast<int32_t *>(&yosemitePatchReplace[YosemitePatchOff]) += (0x1080020 - 0x1080004);
+		*reinterpret_cast<int32_t *>(&sierraPatchReplace[SierraPatchOff])     += (0x1080020 - 0x1080004);
+		return true;
+	}
+
+	return false;
 }
 
 static void shikiPatcherLoad(void *, KernelPatcher &) {
-	// We need to do magic here and prepare a patch for AppleGVA that would force
-	// the assumption that NVIDIA is compatible with SKL/KBL processors.
-
-	if (nvPatch) {
-		size_t gvaSize {0};
-		auto gvaBuf = FileIO::readFileToBuffer("/System/Library/PrivateFrameworks/AppleGVA.framework/Versions/A/AppleGVA", gvaSize);
-		if (gvaBuf && gvaSize > PAGE_SIZE) {
-			// We perform a two step search, firstly we find the relevant starting point, then we find the patch place
-			size_t currOff {0}, endOff {0};
-
-			// The real patch place should be very close
-			static constexpr size_t MaxPatchOff {128};
-
-			// 10.12 and newer support many Intel GPUs, which makes the check place very traceable
-			if (getKernelVersion() > KernelVersion::ElCapitan) {
-				// We need to find the initial check, which we expect to be or with stack var, because of the following:
-				// - there are several GPUs that we need to test against (2xSKL, 2xKBL, thus not enough registers)
-				// - the check is built as one of Intel and AMD (thus a number of ors in a row)
-				static constexpr uint8_t OrOpcode[] {0xFF, 0xFF, 0x48, 0x0B /* XX, XX, XX, XX, XX */};
-
-				endOff = gvaSize - sizeof(OrOpcode) + 5;
-				while (currOff != endOff && memcmp(&gvaBuf[currOff], OrOpcode, sizeof(OrOpcode)))
-					currOff++;
-
-				if (currOff != endOff && endOff - currOff > MaxPatchOff)
-					DBGLOG("shiki", "found pre or instructions at %lu (%02X %02X %02X %02X %02X %02X %02X)", currOff, gvaBuf[currOff+2],
-						   gvaBuf[currOff+3], gvaBuf[currOff+4], gvaBuf[currOff+5], gvaBuf[currOff+6], gvaBuf[currOff+7], gvaBuf[currOff+8]);
-
-				else
-					currOff = endOff = 0;
-			} else {
-				// 10.11 is much more problematic, but the good thing is it no longer updates, we use an idea of
-				// - few Intel GPUs
-				// - test and jmp taking 3 and 2 bytes correspondingly
-				static constexpr uint8_t TestOpcode[] {0x74, 0x05, 0x4D, 0x85 /* XX */};
-
-				endOff = gvaSize - sizeof(TestOpcode) + 1;
-				while (currOff != endOff && memcmp(&gvaBuf[currOff], TestOpcode, sizeof(TestOpcode)))
-					currOff++;
-
-				if (currOff != endOff && endOff - currOff > MaxPatchOff)
-					DBGLOG("shiki", "found pre test instructions at %lu (%02X %02X %02X %02X %02X)", currOff, gvaBuf[currOff],
-						   gvaBuf[currOff+1], gvaBuf[currOff+2], gvaBuf[currOff+3], gvaBuf[currOff+4]);
-
-				else
-					currOff = endOff = 0;
-			}
-
-			if (currOff && endOff) {
-				// Now we need to get to 2 jmps, which we expect to be 0F 84 xx xx xx xx, because of the following:
-				// - acceleration availability takes the main branch, and its code is large (thus 4 byte relative jmp)
-				// - we have non-zero comparators (thus je will be used)
-				static constexpr size_t JmpOpcodeSize {6};
-				static constexpr size_t JmpOpcodeNum {2};
-				static constexpr uint8_t JmpOpcode[] {0x0F, 0x84 /* XX, XX, XX, XX */};
-
-				size_t jmpOffsets[JmpOpcodeNum] {};
-				bool jmpOffsetsOk {true};
-
-				endOff = currOff + MaxPatchOff - NVPatchSize;
-
-				for (size_t i = 0; i < arrsize(jmpOffsets); i++) {
-					while (currOff != endOff && memcmp(&gvaBuf[currOff], JmpOpcode, sizeof(JmpOpcode)))
-						currOff++;
-
-					if (currOff != endOff) {
-						DBGLOG("shiki", "found %lu jmp instruction at %lu (%02X %02X %02X %02X %02X %02X)", i, currOff, gvaBuf[currOff],
-							   gvaBuf[currOff+1], gvaBuf[currOff+2], gvaBuf[currOff+3], gvaBuf[currOff+4], gvaBuf[currOff+5]);
-
-						if (i == 0 || (jmpOffsets[0] + (NVPatchSize - JmpOpcodeSize) >= jmpOffsets[i])) {
-							DBGLOG("shiki", "offset %lu is within the bounds", i);
-							jmpOffsets[i] = currOff;
-						} else {
-							jmpOffsetsOk = false;
-						}
-						currOff++;
-					} else {
-						jmpOffsetsOk = false;
-					}
-				}
-
-				if (jmpOffsetsOk && currOff != endOff) {
-					DBGLOG("shiki", "preparing find & replace nvidia buffers");
-					lilu_os_memcpy(nvPatchFind, &gvaBuf[jmpOffsets[0]], NVPatchSize);
-					lilu_os_memcpy(nvPatchReplace, &gvaBuf[jmpOffsets[0]], NVPatchSize);
-
-					static constexpr uint8_t NopOpcode {0x90};
-					for (size_t i = 0; i < arrsize(jmpOffsets); i++)
-						memset(&nvPatchReplace[jmpOffsets[i] - jmpOffsets[0]], NopOpcode, JmpOpcodeSize);
-
-					DBGLOG("shiki", "find %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X", nvPatchFind[0], nvPatchFind[1], nvPatchFind[2],
-						   nvPatchFind[3], nvPatchFind[4], nvPatchFind[5], nvPatchFind[6], nvPatchFind[7], nvPatchFind[8], nvPatchFind[9]);
-
-					DBGLOG("shiki", "repl %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X", nvPatchReplace[0], nvPatchReplace[1], nvPatchReplace[2],
-						   nvPatchReplace[3], nvPatchReplace[4], nvPatchReplace[5], nvPatchReplace[6], nvPatchReplace[7], nvPatchReplace[8], nvPatchReplace[9]);
-
-					// Ok, everything looks sane enough
-					nvPatch->section = SectionNVDA;
-				}
-			} else {
-				SYSLOG("shiki", "unable to find or instructions");
-			}
-		} else {
-			SYSLOG("shiki", "failed to read AppleGVA framework %lu", gvaSize);
-		}
-
-		if (gvaBuf) Buffer::deleter(gvaBuf);
-	}
-
 	if (autodetectIGPU) {
-		auto frame = getModernFrameId();
-		DBGLOG("shiki", "detected igpu frame 0x%08x", frame);
+		auto frame = CPUInfo::getGpuPlatformId();
 		// Older IGPUs (and invalid frames) get no default hacks
-		if (!frame)
+		if (frame == CPUInfo::DefaultInvalidPlatformId)
 			disableSection(SectionKEGVA);
 	}
 
@@ -200,93 +182,76 @@ static void shikiPatcherLoad(void *, KernelPatcher &) {
 }
 
 static void shikiStart() {
-	// Attempt to support fps.1_0 in Safari
+	bool forceOnlineRenderer     = false;
+	bool allowNonBGRA            = false;
+	bool forceCompatibleRenderer = false;
+	bool addExecutableWhitelist  = false;
+	bool disableKeyExchange      = false;
+	bool replaceBoardID          = false;
+	bool unlockFP10Streaming     = false;
+
 	int bootarg {0};
-	bool patchStreamVideo = PE_parse_boot_argn("-shikifps", &bootarg, sizeof(bootarg));
-	bool forceAccelRenderer = false;
-	bool allowNonBGRA = false;
-	bool forceNvidiaUnlock = false;
-	bool addExeWhiteList = false;
-	bool killKeyExchange = false;
-	bool replaceBoardID = false;
-	
 	if (PE_parse_boot_argn("shikigva", &bootarg, sizeof(bootarg))) {
-		forceAccelRenderer = bootarg & ForceOnlineRenderer;
-		allowNonBGRA       = bootarg & AllowNonBGRA;
-		forceNvidiaUnlock  = bootarg & ForceCompatibleRenderer;
-		addExeWhiteList    = bootarg & VDAExecutableWhitelist;
-		killKeyExchange    = bootarg & DisableHardwareKeyExchange;
-		replaceBoardID     = bootarg & ReplaceBoardID;
+		forceOnlineRenderer     = bootarg & ForceOnlineRenderer;
+		allowNonBGRA            = bootarg & AllowNonBGRA;
+		forceCompatibleRenderer = bootarg & ForceCompatibleRenderer;
+		addExecutableWhitelist  = bootarg & AddExecutableWhitelist;
+		disableKeyExchange      = bootarg & DisableHardwareKeyExchange;
+		replaceBoardID          = bootarg & ReplaceBoardID;
+		unlockFP10Streaming     = bootarg & UnlockFP10Streaming;
 	} else {
-		// By default enable iTunes hack for 10.13 and higher for Ivy+ IGPU
-		killKeyExchange = autodetectIGPU = getKernelVersion() >= KernelVersion::HighSierra;
+		// By default enable iTunes hack for 10.13 and higher for Sandy+ IGPUs
+		disableKeyExchange = autodetectIGPU = getKernelVersion() >= KernelVersion::HighSierra;
 
 		if (PE_parse_boot_argn("-shikigva", &bootarg, sizeof(bootarg))) {
-			SYSLOG("shiki", "-shikigva is deprecated use shikigva=%d instead", ForceOnlineRenderer);
-			forceAccelRenderer = true;
+			SYSLOG("shiki", "-shikigva is deprecated use shikigva %d bit instead", ForceOnlineRenderer);
+			forceOnlineRenderer = true;
 		}
 	}
 
-	DBGLOG("shiki", "stream %d accel %d bgra %d nvidia %d/%d ke1 %d id %d", patchStreamVideo, forceAccelRenderer,
-		   allowNonBGRA, forceNvidiaUnlock, addExeWhiteList, killKeyExchange, replaceBoardID);
+	if (PE_parse_boot_argn("-shikifps", &bootarg, sizeof(bootarg))) {
+		SYSLOG("shiki", "-shikifps is deprecated use shikigva %d bit instead", UnlockFP10Streaming);
+		unlockFP10Streaming = true;
+	}
 
-	// Disable unused SectionFSTREAM
-	if (!patchStreamVideo)
-		disableSection(SectionNSTREAM);
-	
-	if (!forceAccelRenderer)
+	DBGLOG("shiki", "config: online %d, bgra %d, compat %d, whitelist %d, ke1 %d, id %d, stream %d", forceOnlineRenderer,
+		allowNonBGRA, forceCompatibleRenderer, addExecutableWhitelist, disableKeyExchange, replaceBoardID, unlockFP10Streaming);
+
+	// Disable unused sections
+	if (!forceOnlineRenderer)
 		disableSection(SectionOFFLINE);
-	
+
 	if (!allowNonBGRA)
 		disableSection(SectionBGRA);
 
-	if (!replaceBoardID) {
-		disableSection(SectionBOARDID);
-	} else {
+	// Compatible renderer patch varies depending on the CPU.
+	if (!forceCompatibleRenderer || !shikiSetCompatibleRendererPatch())
+		disableSection(SectionCOMPATRENDERER);
+
+	if (!addExecutableWhitelist)
+		disableSection(SectionWHITELIST);
+	
+	if (!disableKeyExchange)
+		disableSection(SectionKEGVA);
+
+	// Custom board-id may be overridden by a boot-arg
+	if (replaceBoardID) {
 		if (!PE_parse_boot_argn("shiki-id", customBoardID, sizeof(customBoardID)))
 			snprintf(customBoardID, sizeof(customBoardID), "Mac-27ADBB7B4CEE8E61"); // iMac14,2
 		DBGLOG("shiki", "requesting %s board-id for gva", customBoardID);
+	} else {
+		disableSection(SectionBOARDID);
 	}
 
-	// Do not enable until we are certain it works
-	bool requireNvidiaPatch = forceNvidiaUnlock && getKernelVersion() >= KernelVersion::ElCapitan;
-	if (requireNvidiaPatch) {
-		for (size_t i = 0; i < ADDPR(binaryModSize); i++) {
-			auto patches = ADDPR(binaryMod)[i].patches;
-			for (size_t j = 0; j < ADDPR(binaryMod)[i].count; j++) {
-				if (patches[j].section == SectionNVDA) {
-					nvPatch = &patches[j];
-					DBGLOG("shiki", "found nvidia patch at %lu:%lu with size %lu", i, j, nvPatch->size);
-					break;
-				}
-			}
-		}
+	if (!unlockFP10Streaming)
+		disableSection(SectionNSTREAM);
 
-		if (nvPatch) {
-			nvPatch->section = SectionUnused;
-			nvPatch->find    = nvPatchFind;
-			nvPatch->replace = nvPatchReplace;
-			nvPatch->size    = NVPatchSize;
-		} else {
-			SYSLOG("shiki", "failed to find nvidia patch though requested");
-			requireNvidiaPatch = false;
-		}
-	}
-
-	bool patcherLoadOk = false;
-	if (requireNvidiaPatch || autodetectIGPU || replaceBoardID) {
+	// Schedule onPatcherLoad if we need to autodetect IGPU or to set a custom board-id
+	if (autodetectIGPU || replaceBoardID) {
 		auto err = lilu.onPatcherLoad(shikiPatcherLoad);
 		if (err == LiluAPI::Error::NoError)
-			patcherLoadOk = true;
-		else
 			SYSLOG("shiki", "unable to attach to patcher load %d", err);
 	}
-
-	if (!addExeWhiteList || !patcherLoadOk)
-		disableSection(SectionNVDA);
-
-	if (!killKeyExchange || (autodetectIGPU && !patcherLoadOk))
-		disableSection(SectionKEGVA);
 
 	auto err = lilu.onProcLoad(ADDPR(procInfo), ADDPR(procInfoSize), nullptr, nullptr, ADDPR(binaryMod), ADDPR(binaryModSize));
 	if (err != LiluAPI::Error::NoError)
